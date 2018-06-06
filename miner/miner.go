eaea11102ebc9a08135a929a8db845627088af97
liangzhijian-aichain <liangzhijian@aichain.me>
2018-06-06 14:44:08 +0800

    Add balance check and sync status check.

--- miner/miner.go
+++ miner/miner.go
@@ -19,7 +19,10 @@ package miner
 
 import (
 	"fmt"
+	"math/big"
+	"os"
 	"sync/atomic"
+	"time"
 
 	"github.com/AICHAIN-CORE/go-aichain/accounts"
 	"github.com/AICHAIN-CORE/go-aichain/common"
@@ -31,6 +34,7 @@ import (
 	"github.com/AICHAIN-CORE/go-aichain/ethdb"
 	"github.com/AICHAIN-CORE/go-aichain/event"
 	"github.com/AICHAIN-CORE/go-aichain/log"
+	"github.com/AICHAIN-CORE/go-aichain/p2p"
 	"github.com/AICHAIN-CORE/go-aichain/params"
 )
 
@@ -40,6 +44,8 @@ type Backend interface {
 	BlockChain() *core.BlockChain
 	TxPool() *core.TxPool
 	ChainDb() ethdb.Database
+	Downloader() *downloader.Downloader // add for check peers num before start mining!!!
+	NodeServer() *p2p.Server
 }
 
 // Miner creates blocks and searches for proof-of-work values.
@@ -108,10 +114,46 @@ func (self *Miner) Start(coinbase common.Address) {
 	self.worker.setEtherbase(coinbase)
 	self.coinbase = coinbase
 
+	stateDb, err := self.eth.BlockChain().StateAt(self.eth.BlockChain().CurrentBlock().Root())
+	if err == nil {
+		balance := stateDb.GetBalance(coinbase)
+		if !self.eth.BlockChain().Config().CheckMinerAccountAit(balance) {
+			fmt.Printf("Not enough AIT for the miner account, %s AIT needed, balance of current miner account: %s AIT.\n", self.eth.BlockChain().Config().AitNeedForMinerAccount().Text(10), balance.Div(balance, big.NewInt(1e+18)).Text(10))
+			os.Exit(1)
+		}
+	} else {
+		log.Error("get coinbase balance error\n", "err", err)
+		os.Exit(1)
+	}
+
 	if atomic.LoadInt32(&self.canStart) == 0 {
 		log.Info("Network syncing, will start miner afterwards")
 		return
 	}
+
+	for {
+		npeers := self.eth.NodeServer().PeerCount()
+		fmt.Printf("Can not start miner, the miner must have peers. Current peers num=%d\n", npeers)
+		if npeers <= 0 {
+			log.Info("Error: There is no peers connected!")
+			time.Sleep(10 * time.Second)
+		} else {
+			break
+		}
+	}
+
+	var (
+		syncing bool
+	)
+
+	sync := self.eth.Downloader().Progress()
+	syncing = self.eth.BlockChain().CurrentHeader().Number.Uint64() >= sync.HighestBlock
+	if syncing {
+	} else {
+		log.Info("Error: Node need sync blockchain data before start mining!")
+		return
+	}
+
 	atomic.StoreInt32(&self.mining, 1)
 
 	log.Info("Starting mining operation")
@@ -180,4 +222,8 @@ func (self *Miner) PendingBlock() *types.Block {
 func (self *Miner) SetEtherbase(addr common.Address) {
 	self.coinbase = addr
 	self.worker.setEtherbase(addr)
+}
+
+func (self *Miner) SetEtherbasePassphrase(passphrase string) {
+	self.worker.SetEtherbasePassphrase(passphrase)
 }
