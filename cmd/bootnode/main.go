82aa27ae445a410fe4fe9a69b81f293775a7c948
duankai <duankai@aichainnews.com>
2019-01-15 10:37:25 +0800

    Replace the p2p dicover with enode, synced code with internal git.

--- cmd/bootnode/main.go
+++ cmd/bootnode/main.go
@@ -29,6 +29,7 @@ import (
 	"github.com/AICHAIN-CORE/go-aichain/log"
 	"github.com/AICHAIN-CORE/go-aichain/p2p/discover"
 	"github.com/AICHAIN-CORE/go-aichain/p2p/discv5"
+	"github.com/AICHAIN-CORE/go-aichain/p2p/enode"
 	"github.com/AICHAIN-CORE/go-aichain/p2p/nat"
 	"github.com/AICHAIN-CORE/go-aichain/p2p/netutil"
 )
@@ -85,7 +86,7 @@ func main() {
 	}
 
 	if *writeAddr {
-		fmt.Printf("%v\n", discover.PubkeyID(&nodeKey.PublicKey))
+		fmt.Printf("%v\n", enode.PubkeyToIDV4(&nodeKey.PublicKey))
 		os.Exit(0)
 	}
 
