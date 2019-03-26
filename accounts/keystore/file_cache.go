3eedf61cb3bab8eb139292b40276c5499716e173
duankai <duankai@aichainnews.com>
2019-03-26 10:18:54 +0800

    Synced the subdir accounts/keystore source code with internal git.

--- accounts/keystore/file_cache.go
+++ accounts/keystore/file_cache.go
@@ -31,7 +31,7 @@ import (
 // fileCache is a cache of files seen during scan of keystore.
 type fileCache struct {
 	all     mapset.Set // Set of all files from the keystore folder
-	lastMod time.Time     // Last time instance when a file was modified
+	lastMod time.Time  // Last time instance when a file was modified
 	mu      sync.RWMutex
 }
 
